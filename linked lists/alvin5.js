6b230801977c27b3065083876a27a3f675123f1e
Jean <jeancarlossenpai@gmail.com>
2022-06-20 20:44:10 -0400

    Finish reverse

--- linked lists/alvin5.js
+++ linked lists/alvin5.js
@@ -16,15 +16,25 @@ a.next = b
 b.next = c
 c.next = d
 
+
+console.log(reverse_r(a))
+// https://youtu.be/Hj_rA0dhr2I 59:00
+function reverse_r(head, prev = null){
+    if (head === null) return prev
+    const next = head.next
+    head.next = prev
+    return reverse_r(next, head)
+}
 function reverse(head){
     let currentValue = head
     let prev = null
-    let next = currentValue.next
 
     while(currentValue){
-        
+        const next  = currentValue.next
         currentValue.next = prev
         prev = currentValue
-        currentValue = currentValue.next
+        currentValue = next
     }
+
+    return prev
 }
