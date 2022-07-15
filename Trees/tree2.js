afbd048a83a6927a695e5d37dc28128639b2ad14
Jeankai <jfarias@indrhi.local>
2022-07-15 14:34:50 -0400

    BST

--- Trees/tree2.js
+++ Trees/tree2.js
@@ -12,47 +12,143 @@ class MyTree{
       }
 
       insert(value){
-        const newNode = new Node(value)
-        let current = this.root
-        if (!current) {
-            this.root = new Node(value)
-            return this
-        }
-
-        while (current) {
-            if(value === current.value) return undefined;
-
-            if (current.value > value) { // Check if the value goes left or right
-                 if (current.right === null) {
-                    current.right = newNode
-                    return this
-                 }
-                 current = current.right
-            } else{
-                if (current.left === null) {
-                    current.left = newNode
-                    return this
-                }
-
-                current = current.left
+        const newNode = new Node(value);
+        if (this.root === null) {
+          this.root = newNode;
+        } else {
+          let currentNode = this.root;
+          while(true){
+            if(value < currentNode.value){
+              //Left
+              if(!currentNode.left){
+                currentNode.left = newNode;
+                return this;
+              }
+              currentNode = currentNode.left;
+            } else {
+              //Right
+              if(!currentNode.right){
+                currentNode.right = newNode;
+                return this;
+              } 
+              currentNode = currentNode.right;
             }
-
-        } //END WHILE
+          }
+        }
       }
+    
+    lookup(value){
+       if(!this.root) return false
+
+       let current = this.root
+
+       while (current) {
+        if (value< current.value) {
+            current = current.left
+        } else if(value > current.value){
+            current = current.right
+        } else if (value === current.value){
+            return current
+        }
+       }
+
+       return false
+    }
+
+
+    remove(value) {
+      if (!this.root) {
+        return false;
+      }
+      let currentNode = this.root;
+      let parentNode = null;
+      while(currentNode){
+        if(value < currentNode.value){
+          parentNode = currentNode;
+          currentNode = currentNode.left;
+        } else if(value > currentNode.value){
+          parentNode = currentNode;
+          currentNode = currentNode.right;
+        } else if (currentNode.value === value) {
+          //We have a match, get to work!
+          
+          //Option 1: No right child: 
+          if (currentNode.right === null) {
+            if (parentNode === null) {
+              this.root = currentNode.left;
+            } else {
+              
+              //if parent > current value, make current left child a child of parent
+              if(currentNode.value < parentNode.value) {
+                parentNode.left = currentNode.left;
+              
+              //if parent < current value, make left child a right child of parent
+              } else if(currentNode.value > parentNode.value) {
+                parentNode.right = currentNode.left;
+              }
+            }
+          
+          //Option 2: Right child which doesnt have a left child
+          } else if (currentNode.right.left === null) {
+            currentNode.right.left = currentNode.left;
+            if(parentNode === null) {
+              this.root = currentNode.right;
+            } else {
+              
+              //if parent > current, make right child of the left the parent
+              if(currentNode.value < parentNode.value) {
+                parentNode.left = currentNode.right;
+              
+              //if parent < current, make right child a right child of the parent
+              } else if (currentNode.value > parentNode.value) {
+                parentNode.right = currentNode.right;
+              }
+            }
+          
+          //Option 3: Right child that has a left child
+          } else {
+  
+            //find the Right child's left most child
+            let leftmost = currentNode.right.left;
+            let leftmostParent = currentNode.right;
+            while(leftmost.left !== null) {
+              leftmostParent = leftmost;
+              leftmost = leftmost.left;
+            }
+            
+            //Parent's left subtree is now leftmost's right subtree
+            leftmostParent.left = leftmost.right;
+            leftmost.left = currentNode.left;
+            leftmost.right = currentNode.right;
+  
+            if(parentNode === null) {
+              this.root = leftmost;
+            } else {
+              if(currentNode.value < parentNode.value) {
+                parentNode.left = leftmost;
+              } else if(currentNode.value > parentNode.value) {
+                parentNode.right = leftmost;
+              }
+            }
+          }
+        return true;
+        }
+      }
+    }
+  
+
 }
 
 const tree = new MyTree()
 
+for (const element of [8,4,12,2,6,10,14,16,1,3,19,15]) {
+    tree.insert(element)
+    }
 
-console.log(tree.root)
-tree.insert(8)
-tree.insert(4)
-tree.insert(12)
-tree.insert(2)
-tree.insert(6)
-tree.insert(10)
-tree.insert(14)
-// tree.insert(16)
-console.log(tree)
+// console.log(tree)
+
+console.log(tree.lookup(16));
+tree.remove(16)
+console.log(tree.lookup(16));
 
 console.log(JSON.stringify(tree))
